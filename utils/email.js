f093ac9234aa89050fe40d0a1677030d573712d1
MINDI KINDI <WI@users-Mac-Pro.local>
2025-04-20 13:57:05 +0700

    fix_email

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,24 @@
 const nodemailer = require('nodemailer');
 
-// Configure Mailgun SMTP settings
 const transporter = nodemailer.createTransport({
-  host: 'smtp.mailgun.org', // SMTP hostname
-  port: 587,               // Recommended SMTP port
-  secure: false,           // Use TLS (false for STARTTLS)
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false, // Harus false untuk port 587
   auth: {
-    user: 'postmaster@sandboxdf83a25315564e2585b59a472c2db530.mailgun.org', // Your Mailgun SMTP username
-    pass: '8fcb94a0b29902be4e242c19861d30e8-0920befd-7920ae6d',             // Your Mailgun SMTP password
+    user: '8af2ce001@smtp-brevo.com',
+    pass: 'kDB5frs0MOZ6xpgz',
   },
 });
 
 const sendEmail = async (to, subject, text) => {
-  const mailOptions = {
-    from: 'Ingatan Gajah <postmaster@sandboxdf83a25315564e2585b59a472c2db530.mailgun.org>', // Replace 'YourName' with your desired sender name
-    to, // Recipient email address
-    subject, // Email subject
-    text, // Email text content
-  };
-
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent:', info.messageId);
+    await transporter.sendMail({
+      from: '"Ingatangajah" <8af2ce001@smtp-brevo.com>', // nama pengirim
+      to,
+      subject,
+      text,
+    });
+    console.log('Email sent successfully');
   } catch (error) {
     console.error('Error sending email:', error);
     throw error;
