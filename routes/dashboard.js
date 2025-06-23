1a53522e09d42e7a3598a7752434255bdd4d0214
arifzamzam <arifzamzam.id@gmail.com>
2025-06-23 23:24:46 +0700

    fix code

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,17 +5,17 @@ const pool = require('../db');
 router.get('/student-per-package', async (_req, res) => {
   try {
     const result = await pool.query(`SELECT p.name AS package_name, COUNT(se.student_id) AS total_students
-                                     FROM student_enroll se
+                                     FROM student_enrollments se
                                      JOIN packages p ON se.package_id = p.id
                                      GROUP BY se.package_id, p.name;`);
     res.status(200).json(result.rows);
   } catch (error) {
-    console.error('Error fetching holidays:', error);
-    res.status(500).json({ error: 'Failed to fetch holidays' });
+    console.error('Error fetching total student:', error);
+    res.status(500).json({ error: 'Failed to fetch total student' });
   }
 });
 
-router.get('/total-active-student', async (_req, res) => {
+router.get('/total-active-student', async (req, res) => {
     try {
         const now = new Date();
         const start_date = req.query.start_date || new Date(now.getFullYear(), now.getMonth(), 1).toISOString().slice(0, 10);
