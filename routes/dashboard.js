ec4ec29d5fc0bc21e89319ae8a87829b22ad2ed6
arifzamzam <arifzamzam.id@gmail.com>
2025-06-23 16:28:45 +0700

    fix dashboard api

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const { addDurationToTime } = require('../utils/helper');
 
 router.get('/student-per-package', async (_req, res) => {
   try {
@@ -42,3 +41,4 @@ router.get('/total-active-student', async (_req, res) => {
 });
 
 
+module.exports = router;
