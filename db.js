004aeb931169c361da3a4fba81f5e58386a230bf
arifzamzam <arifzamzam.id@gmail.com>
2025-07-12 22:27:40 +0700

    update db.js

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const { Pool } = require('pg');
 
+console.log('Connecting to:', process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false, // Neon requires SSL, but disable cert verification
+  },
 });
 
 module.exports = pool;
