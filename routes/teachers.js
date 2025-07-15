116fde8341ff6a7ced0fff4f3888adea8e01cd4e
arifzamzam <arifzamzam.id@gmail.com>
2025-07-15 10:57:45 +0700

    add branch_teachers

--- routes/teachers.js
+++ routes/teachers.js
@@ -9,8 +9,8 @@ router.post('/', async (req, res) => {
     const {
       full_name, email, birthdate, nickname, gender,
       phone_number, address, city_id, city_name,
-      province_id, province_name, branch_id,
-      package_id, last_education_place, daily_language, join_with_ig, education_level, role,
+      province_id, province_name, branch_ids,
+      package_ids, last_education_place, daily_language, join_with_ig, education_level, role,
       url_registration_code
     } = req.body;
 
@@ -34,15 +34,15 @@ router.post('/', async (req, res) => {
     const result = await pool.query(
       `INSERT INTO teachers (
         full_name, birthdate, nickname, gender, phone_number, address, city_id, city_name,
-        province_id, province_name, users_id, branch_id, last_education_place,
+        province_id, province_name, users_id, last_education_place,
         daily_language, join_with_ig, education_level, role, url_registration_code
       ) VALUES (
         $1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
-        $11, $12, $13, $14, $15, $16, $17, $18
+        $11, $12, $13, $14, $15, $16, $17
       ) RETURNING *`,
       [
         full_name, birthdate, nickname, gender, phone_number, address, city_id, city_name,
-        province_id, province_name, users_id, branch_id, last_education_place,
+        province_id, province_name, users_id, last_education_place,
         daily_language, join_with_ig, education_level, role, url_registration_code
       ]
     );
@@ -56,6 +56,13 @@ router.post('/', async (req, res) => {
       );
     }
 
+    // Insert ke branch_teacher
+    if (branch_ids && branch_ids.length > 0) {
+      const insertValues = package_ids.map((pkgId) => `(${teacher.id}, ${pkgId})`).join(',');
+      await pool.query(
+        `INSERT INTO package_teacher (teacher_id, package_id) VALUES ${insertValues}`
+      );
+    }
     res.status(201).json(teacher);
   } catch (err) {
     console.error('Error creating teacher:', err);
@@ -94,12 +101,13 @@ router.get('/', async (req, res) => {
       u.email,
       u.role_id,
       r.name AS role_name,
-      b.name AS branch_name,
-      string_agg(p.name, ', ') AS package_names
+      string_agg(p.name, ', ') AS package_names,
+      string_agg(b.name, ', ') AS branch_names
     FROM teachers t
     LEFT JOIN users u ON t.users_id = u.id
     LEFT JOIN roles r ON u.role_id = r.id
-    LEFT JOIN branches b ON t.branch_id = b.id
+    LEFT JOIN branch_teacher bt ON bt.teacher_id = t.id
+    LEFT JOIN branches b ON bt.branch_id = b.id
     LEFT JOIN package_teacher pt ON pt.teacher_id = t.id
     LEFT JOIN packages p ON pt.package_id = p.id
     WHERE ${whereClause}
@@ -154,10 +162,12 @@ const { page = 1, search, limit } = req.query;
       r.name AS role_name,
       b.name AS branch_name,
       string_agg(p.name, ', ') AS package_names
+      string_agg(b.name, ', ') AS branch_names
     FROM teachers t
     LEFT JOIN users u ON t.users_id = u.id
     LEFT JOIN roles r ON u.role_id = r.id
-    LEFT JOIN branches b ON t.branch_id = b.id
+    LEFT JOIN branch_teacher bt ON bt.teacher_id = t.id
+    LEFT JOIN branches b ON bt.branch_id = b.id
     LEFT JOIN package_teacher pt ON pt.teacher_id = t.id
     LEFT JOIN packages p ON pt.package_id = p.id
     WHERE ${whereClause}
@@ -185,16 +195,17 @@ router.get('/:id', async (req, res) => {
     const result = await pool.query(`SELECT 
         t.*,
         u.email,
-        b.name AS branch_name,
+        string_agg(b.name, ', ') AS branch_names,
         string_agg(p.name, ', ') AS package_names
       FROM teachers t
       LEFT JOIN users u ON t.users_id = u.id
       LEFT JOIN roles r ON u.role_id = r.id
-      LEFT JOIN branches b ON t.branch_id = b.id
+      LEFT JOIN branch_teacher bt ON bt.teacher_id = t.id
+      LEFT JOIN branches b ON bt.branch_id = b.id
       LEFT JOIN package_teacher pt ON pt.teacher_id = t.id
       LEFT JOIN packages p ON pt.package_id = p.id
       WHERE t.id = $1
-      GROUP BY t.id, u.email, u.role_id, r.name, b.name;`, [req.params.id]);
+      GROUP BY t.id, u.email, u.role_id, r.name, b.name`, [req.params.id]);
     if (result.rows.length === 0) return res.status(404).json({ error: 'Teacher not found' });
     res.status(200).json(result.rows[0]);
   } catch (err) {
